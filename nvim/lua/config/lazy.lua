f29a552280934448eadb0c2c6111b835e1c6d7d1
ginn1111 <vanthuanjw@gmail.com>
2024-01-15 12:56:53 +0700

    feat: change dashboard config

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -13,10 +13,10 @@ require("lazy").setup({
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
-    { import = "lazyvim.plugins.extras.ui.alpha" },
+    -- { import = "lazyvim.plugins.extras.util.project" },
+    { import = "lazyvim.plugins.extras.util.project" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.

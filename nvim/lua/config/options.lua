ea1bc2925b900b19595bbb76d6bf1df10b5654ce
ginn1111 <vanthuanjw@gmail.com>
2024-01-29 21:10:59 +0700

    feat: config noice, inclie, scrollbar, inc-rename

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,11 +1,18 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
--- key binding mapleader
 vim.g.mapleader = ";"
 
 local opt = vim.opt
 opt.winbar = "%=%m %f"
+opt.spell = true
+opt.spelllang = { "en_us" }
+
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+-- vim.cmd(
+--   [[hi SpellBad   guisp=red    gui=undercurl guifg=NONE guibg=NONE ctermfg=NONE ctermbg=NONE term=underline cterm=undercurl ctermul=red]]
+-- )
+-- vim.cmd(
+--   [[hi SpellCap   guisp=yellow    gui=undercurl guifg=NONE guibg=NONE ctermfg=NONE ctermbg=NONE term=underline cterm=undercurl ctermul=yellow]]
+-- )
 
 -- indentation
 opt.foldlevelstart = 99

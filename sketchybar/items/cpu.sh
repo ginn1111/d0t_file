ddf26ac48c9f88d780e23473a092ed809e5cc853
Thuận Phạm <thuanpv@192.168.1.19>
2024-11-04 08:19:57 +0700

    tmp

--- sketchybar/items/cpu.sh
+++ sketchybar/items/cpu.sh
@@ -8,16 +8,15 @@
 
 FONT="Hack Nerd Font"
 
-sketchybar --add alias "Stats,RAM_mini" right \
-  --set "Stats,RAM_mini" update_freq=2 \
-  icon.padding_right=2 icon.font="$FONT:Regular:20.0" icon.color=$YELLOW label.drawing=off \
-  icon=󰟖
-sketchybar --add alias "Stats,CPU_mini" right \
-  --set "Stats,CPU_mini" update_freq=2 \
-  icon.padding_right=0 icon.font="$FONT:Regular:18.0" icon.color=$ORANGE label.drawing=off \
-  icon=󰍛
-
-sketchybar --add alias "Stats,Sensors_sensors" right \
+sketchybar --add alias "Stats,Sensors_sensors" e \
   --set "Stats,Sensors_sensors" update_freq=2 \
   icon.padding_right=0 icon.font="$FONT:Regular:16.0" icon.color=$RED label.drawing=off \
   icon=
+sketchybar --add alias "Stats,CPU_mini" e \
+  --set "Stats,CPU_mini" update_freq=2 \
+  icon.padding_right=0 icon.font="$FONT:Regular:18.0" icon.color=$ORANGE label.drawing=off \
+  icon=󰍛
+sketchybar --add alias "Stats,RAM_mini" e \
+  --set "Stats,RAM_mini" update_freq=2 \
+  icon.padding_right=2 icon.font="$FONT:Regular:20.0" icon.color=$YELLOW label.drawing=off \
+  icon=󰘚

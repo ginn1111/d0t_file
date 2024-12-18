8d6d0030851d2a7c6bf771f33a73c9ae78ea6c7f
ginn1111 <vanthuanjw@gmail.com>
2024-12-19 06:02:09 +0700

    feat: update theme for kitty and neovim

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -36,176 +36,176 @@ return {
       },
     },
 
-    highlights = {
-      background = {
-        bg = "#011627",
-      },
-      info = {
-        sp = "#fc514e",
-        bg = "#011627",
-      },
-      info_diagnostic = {
-        sp = "#fc514e",
-        bg = "#011627",
-      },
-      hint_diagnostic = {
-        sp = "#fc514e",
-        bg = "#011627",
-      },
-      error_diagnostic = {
-        sp = "#fc514e",
-        bg = "#011627",
-      },
-      warning_diagnostic = {
-        sp = "#fc514e",
-        bg = "#011627",
-      },
-      diagnostic = {
-        bg = "#011627",
-      },
-      hint = {
-        bg = "#011627",
-      },
-      error = {
-        sp = "#fc514e",
-        bg = "#011627",
-      },
-      warning = {
-        sp = "#fc514e",
-        bg = "#011627",
-      },
-      modified = {
-        bg = "#011627",
-        fg = "#1d3b53",
-      },
-
-      fill = {
-        bg = "#080808",
-      },
-      separator = {
-        fg = "#080808",
-        bg = "#011627",
-      },
-      separator_visible = {
-        fg = "#080808",
-        bg = "#1d3b53",
-      },
-      separator_selected = {
-        fg = "#080808",
-        bg = "#1d3b53",
-      },
-      buffer_visible = {
-        bg = "#1d3b53",
-        fg = "#22C7A8",
-        bold = true,
-        italic = true,
-      },
-      buffer_selected = {
-        bg = "#1d3b53",
-        fg = "#22C7A8",
-        bold = true,
-        italic = true,
-      },
-      modified_visible = {
-        bg = "#1d3b53",
-        fg = "#22C7A8",
-      },
-      modified_selected = {
-        bg = "#1d3b53",
-        fg = "#22C7A8",
-      },
-
-      diagnostic_visible = {
-        bg = "#1d3b53",
-        fg = "#22C7A8",
-      },
-      warning_diagnostic_visible = {
-        bg = "#1d3b53",
-        bold = true,
-        italic = true,
-      },
-      warning_visible = {
-        bg = "#1d3b53",
-        bold = true,
-        italic = true,
-      },
-      error_visible = {
-        bg = "#1d3b53",
-        bold = true,
-        italic = true,
-      },
-
-      hint_diagnostic_visible = {
-        bg = "#1d3b53",
-      },
-      hint_visible = {
-        bg = "#1d3b53",
-      },
-
-      warning_selected = {
-        bg = "#1d3b53",
-        bold = true,
-        italic = true,
-      },
-      error_selected = {
-        bg = "#1d3b53",
-        bold = true,
-        italic = true,
-      },
-
-      hint_selected = {
-        bg = "#1d3b53",
-        bold = true,
-        italic = true,
-      },
-      hint_diagnostic_selected = {
-        bg = "#1d3b53",
-        bold = true,
-        italic = true,
-      },
-      diagnostic_selected = {
-        bg = "#1d3b53",
-        bold = true,
-        italic = true,
-      },
-      warning_diagnostic_selected = {
-        bg = "#1d3b53",
-        bold = true,
-        italic = true,
-      },
-
-      error_diagnostic_visible = {
-        bg = "#1d3b53",
-        bold = true,
-        italic = true,
-      },
-      error_diagnostic_selected = {
-        bg = "#1d3b53",
-        bold = true,
-        italic = true,
-      },
-
-      info_visible = {
-        bg = "#1d3b53",
-      },
-      info_selected = {
-        bg = "#1d3b53",
-        bold = true,
-        italic = true,
-      },
-      info_diagnostic_visible = {
-        bg = "#1d3b53",
-      },
-      info_diagnostic_selected = {
-        bg = "#1d3b53",
-        bold = true,
-        italic = true,
-      },
-
-      trunc_marker = {
-        bg = "#1d3b53",
-        fg = "#22C7A8",
-      },
-    },
+    -- highlights = {
+    --   background = {
+    --     bg = "#011627",
+    --   },
+    --   info = {
+    --     sp = "#fc514e",
+    --     bg = "#011627",
+    --   },
+    --   info_diagnostic = {
+    --     sp = "#fc514e",
+    --     bg = "#011627",
+    --   },
+    --   hint_diagnostic = {
+    --     sp = "#fc514e",
+    --     bg = "#011627",
+    --   },
+    --   error_diagnostic = {
+    --     sp = "#fc514e",
+    --     bg = "#011627",
+    --   },
+    --   warning_diagnostic = {
+    --     sp = "#fc514e",
+    --     bg = "#011627",
+    --   },
+    --   diagnostic = {
+    --     bg = "#011627",
+    --   },
+    --   hint = {
+    --     bg = "#011627",
+    --   },
+    --   error = {
+    --     sp = "#fc514e",
+    --     bg = "#011627",
+    --   },
+    --   warning = {
+    --     sp = "#fc514e",
+    --     bg = "#011627",
+    --   },
+    --   modified = {
+    --     bg = "#011627",
+    --     fg = "#1d3b53",
+    --   },
+    --
+    --   fill = {
+    --     bg = "#080808",
+    --   },
+    --   separator = {
+    --     fg = "#080808",
+    --     bg = "#011627",
+    --   },
+    --   separator_visible = {
+    --     fg = "#080808",
+    --     bg = "#1d3b53",
+    --   },
+    --   separator_selected = {
+    --     fg = "#080808",
+    --     bg = "#1d3b53",
+    --   },
+    --   buffer_visible = {
+    --     bg = "#1d3b53",
+    --     fg = "#22C7A8",
+    --     bold = true,
+    --     italic = true,
+    --   },
+    --   buffer_selected = {
+    --     bg = "#1d3b53",
+    --     fg = "#22C7A8",
+    --     bold = true,
+    --     italic = true,
+    --   },
+    --   modified_visible = {
+    --     bg = "#1d3b53",
+    --     fg = "#22C7A8",
+    --   },
+    --   modified_selected = {
+    --     bg = "#1d3b53",
+    --     fg = "#22C7A8",
+    --   },
+    --
+    --   diagnostic_visible = {
+    --     bg = "#1d3b53",
+    --     fg = "#22C7A8",
+    --   },
+    --   warning_diagnostic_visible = {
+    --     bg = "#1d3b53",
+    --     bold = true,
+    --     italic = true,
+    --   },
+    --   warning_visible = {
+    --     bg = "#1d3b53",
+    --     bold = true,
+    --     italic = true,
+    --   },
+    --   error_visible = {
+    --     bg = "#1d3b53",
+    --     bold = true,
+    --     italic = true,
+    --   },
+    --
+    --   hint_diagnostic_visible = {
+    --     bg = "#1d3b53",
+    --   },
+    --   hint_visible = {
+    --     bg = "#1d3b53",
+    --   },
+    --
+    --   warning_selected = {
+    --     bg = "#1d3b53",
+    --     bold = true,
+    --     italic = true,
+    --   },
+    --   error_selected = {
+    --     bg = "#1d3b53",
+    --     bold = true,
+    --     italic = true,
+    --   },
+    --
+    --   hint_selected = {
+    --     bg = "#1d3b53",
+    --     bold = true,
+    --     italic = true,
+    --   },
+    --   hint_diagnostic_selected = {
+    --     bg = "#1d3b53",
+    --     bold = true,
+    --     italic = true,
+    --   },
+    --   diagnostic_selected = {
+    --     bg = "#1d3b53",
+    --     bold = true,
+    --     italic = true,
+    --   },
+    --   warning_diagnostic_selected = {
+    --     bg = "#1d3b53",
+    --     bold = true,
+    --     italic = true,
+    --   },
+    --
+    --   error_diagnostic_visible = {
+    --     bg = "#1d3b53",
+    --     bold = true,
+    --     italic = true,
+    --   },
+    --   error_diagnostic_selected = {
+    --     bg = "#1d3b53",
+    --     bold = true,
+    --     italic = true,
+    --   },
+    --
+    --   info_visible = {
+    --     bg = "#1d3b53",
+    --   },
+    --   info_selected = {
+    --     bg = "#1d3b53",
+    --     bold = true,
+    --     italic = true,
+    --   },
+    --   info_diagnostic_visible = {
+    --     bg = "#1d3b53",
+    --   },
+    --   info_diagnostic_selected = {
+    --     bg = "#1d3b53",
+    --     bold = true,
+    --     italic = true,
+    --   },
+    --
+    --   trunc_marker = {
+    --     bg = "#1d3b53",
+    --     fg = "#22C7A8",
+    --   },
+    -- },
   },
 }

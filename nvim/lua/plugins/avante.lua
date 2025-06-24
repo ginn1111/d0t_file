360e3f931e7f64e983af440e002236893e8d05d1
Thuận Phạm <thuanpv@192.168.1.3>
2025-06-24 17:17:33 +0700

    chore: update config

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -1,56 +1,76 @@
 return {
-  --   {
-  --     "yetone/avante.nvim",
-  --     event = "VeryLazy",
-  --     lazy = false,
-  --     version = true,
-  --     opts = {
-  --       provider = "deepseek",
-  --       vendors = {
-  --         deepseek = {
-  --           __inherited_from = "openai",
-  --           api_key_name = "sk-fb4407ae748a4b54819764df92cb79b4",
-  --           endpoint = "https://api.deepseek.com",
-  --           model = "deepseek-coder",
-  --         },
-  --       },
-  --     },
-  --     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  --     build = "make",
-  --     -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-  --     dependencies = {
-  --       "nvim-treesitter/nvim-treesitter",
-  --       "stevearc/dressing.nvim",
-  --       "nvim-lua/plenary.nvim",
-  --       "MunifTanjim/nui.nvim",
-  --       --- The below dependencies are optional,
-  --       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-  --       "zbirenbaum/copilot.lua", -- for providers='copilot'
-  --       {
-  --         -- support for image pasting
-  --         "HakonHarnes/img-clip.nvim",
-  --         event = "VeryLazy",
-  --         opts = {
-  --           -- recommended settings
-  --           default = {
-  --             embed_image_as_base64 = false,
-  --             prompt_for_file_name = false,
-  --             drag_and_drop = {
-  --               insert_mode = true,
-  --             },
-  --             -- required for Windows users
-  --             use_absolute_path = true,
-  --           },
-  --         },
-  --       },
-  --       {
-  --         -- Make sure to set this up properly if you have lazy=true
-  --         "MeanderingProgrammer/render-markdown.nvim",
-  --         opts = {
-  --           file_types = { "markdown", "Avante" },
-  --         },
-  --         ft = { "markdown", "Avante" },
-  --       },
-  --     },
-  --   },
+  "yetone/avante.nvim",
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  -- ⚠️ must add this setting! ! !
+  build = function()
+    -- conditionally use the correct build system for the current OS
+    if vim.fn.has("win32") == 1 then
+      return "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+    else
+      return "make"
+    end
+  end,
+  event = "VeryLazy",
+  version = false, -- Never set this value to "*"! Never!
+  ---@module 'avante'
+  ---@type avante.Config
+  -- config = function()
+  --   require("avante").setup({
+  --     base_url = "https://openrouter.ai/api/v1",
+  --     openai_api_key = "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", -- or your actual key
+  --     model = "deepseek-coder", -- or any model you like from OpenRouter
+  --   })
+  -- end,
+  opts = {
+    -- add any opts here
+    -- for example
+    provider = "openrouter",
+    providers = {
+      openrouter = {
+        __inherited_from = "openai",
+        endpoint = "https://openrouter.ai/api/v1",
+        api_key_name = "OPENROUTER_API_KEY",
+        model = "deepseek/deepseek-r1",
+      },
+    },
+  },
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "echasnovski/mini.pick", -- for file_selector provider mini.pick
+    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "ibhagwan/fzf-lua", -- for file_selector provider fzf
+    "stevearc/dressing.nvim", -- for input provider dressing
+    "folke/snacks.nvim", -- for input provider snacks
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
 }

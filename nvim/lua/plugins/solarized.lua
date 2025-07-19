03e465ad2a2533b67c1eb4ace10a451ac90a9f51
ginn1111 <vanthuanjw@gmail.com>
2025-07-19 17:25:34 +0700

    feat: update solarized theme and kitty config

--- nvim/lua/plugins/solarized.lua
+++ nvim/lua/plugins/solarized.lua
@@ -1,79 +1,43 @@
 return {
-  "maxmx03/solarized.nvim",
+  "craftzdog/solarized-osaka.nvim",
   lazy = false,
   priority = 1000,
-  ---@type solarized.config
   opts = {},
-  config = function(_, opts)
-    vim.o.termguicolors = true
-    -- vim.o.background = "light"
-    -- require("solarized").setup(opts)
-
-    require("solarized").setup({
-      transparent = {
-        enabled = true, -- Master switch to enable transparency
-        pmenu = true, -- Popup menu (e.g., autocomplete suggestions)
-        normal = true, -- Main editor window background
-        normalfloat = true, -- Floating windows
-        neotree = true, -- Neo-tree file explorer
-        nvimtree = true, -- Nvim-tree file explorer
-        whichkey = true, -- Which-key popup
-        telescope = true, -- Telescope fuzzy finder
-        lazy = true, -- Lazy plugin manager UI
-        mason = true, -- Mason manage external tooling
-      },
-      on_highlights = nil,
-      on_colors = nil,
-      palette = "solarized", -- solarized (default) | selenized
-      variant = "summer", -- "spring" | "summer" | "autumn" | "winter" (default)
-      error_lens = {
-        text = false,
-        symbol = false,
-      },
+  config = function()
+    require("solarized-osaka").setup({
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      transparent = true, -- Enable this to disable setting the background color
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
       styles = {
-        enabled = true,
-        types = {},
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
         functions = {},
-        parameters = {},
-        comments = {},
-        strings = {},
-        keywords = {},
         variables = {},
-        constants = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
       },
-      plugins = {
-        treesitter = true,
-        lspconfig = true,
-        navic = true,
-        cmp = true,
-        indentblankline = true,
-        neotree = true,
-        nvimtree = true,
-        whichkey = true,
-        dashboard = true,
-        gitsigns = true,
-        telescope = true,
-        noice = true,
-        hop = true,
-        ministatusline = true,
-        minitabline = true,
-        ministarter = true,
-        minicursorword = true,
-        notify = true,
-        rainbowdelimiters = true,
-        bufferline = true,
-        lazy = true,
-        rendermarkdown = true,
-        ale = true,
-        coc = true,
-        leap = true,
-        alpha = true,
-        yanky = true,
-        gitgutter = true,
-        mason = true,
-        flash = true,
-      },
+      sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+      day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+      hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+      dim_inactive = false, -- dims inactive windows
+      lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+      --- You can override specific color groups to use other groups or a hex color
+      --- function will be called with a ColorScheme table
+      ---@param colors ColorScheme
+      on_colors = function(colors) end,
+
+      --- You can override specific highlights to use other groups or a hex color
+      --- function will be called with a Highlights and ColorScheme table
+      ---@param highlights Highlights
+      ---@param colors ColorScheme
+      on_highlights = function(highlights, colors) end,
     })
-    vim.cmd.colorscheme("solarized")
+
+    vim.cmd([[colorscheme solarized-osaka]])
   end,
 }
